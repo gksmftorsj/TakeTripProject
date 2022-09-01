8b40c2941c82ec14ee25c655be7266093d4a1cae
gksmftorsj <ksh990913@naver.com>
2022-09-01 23:18:37 +0900

    수정

--- js/naver-login.js
+++ js/naver-login.js
@@ -2,18 +2,10 @@ var naverLogin = new naver.LoginWithNaverId({
   //내 애플리케이션 정보에 cliendId를 입력해줍니다.
   clientId: "wcyad8RUU4UMX2VrzXe2",
   // 내 애플리케이션 API설정의 Callback URL 을 입력해줍니다.
-<<<<<<< HEAD
   callbackUrl: "https://gksmftorsj.github.io/project0901/",
   isPopup: false, // 팝업을 통한 연동처리 여부
   callbackHandle: true, // callback 페이지가 분리되었을 경우에 callback 페이지에서는 callback 처리를 해줄 수 있도록 설정합니다.02
 });
-=======
-  callbackUrl: "../html/login-signup.html",
-  isPopup: false, // 팝업을 통한 연동처리 여부 
-  callbackHandle: true // callback 페이지가 분리되었을 경우에 callback 페이지에서는 callback 처리를 해줄 수 있도록 설정합니다.02
-}
-);
->>>>>>> c58eede0bfa25f258deb496231d9d1e3fb42c88b
 
 // (3) 네이버아이디 로그인 정보를 초기화하기 위하여 init을 호출
 naverLogin.init();
@@ -22,16 +14,10 @@ const NAVER_USERNAME_KEY = "naver_username";
 const NAVER_EMAIL_KEY = "naver_email";
 
 // (4) Callback의 처리, 정상적으로 Callback 처리가 완료될 경우 main page로 redirect(또는 Popup close) / 페이지가 정상적으로 로드 되었을 때 함수 실행
-<<<<<<< HEAD
 window.addEventListener("load", function () {
   naverLogin.getLoginStatus(function (status) {
     if (status) {
       // 정상적으로 로그인 되면 status = true
-=======
-window.addEventListener('load', function () {
-  naverLogin.getLoginStatus(function (status) {
-    if (status) { // 정상적으로 로그인 되면 status = true
->>>>>>> c58eede0bfa25f258deb496231d9d1e3fb42c88b
       // (5) 필수적으로 받아야 하는 프로필 정보가 있다면 callback처리 시점에 체크
       var username = naverLogin.user.getName(); // 필수정보인 이름 값 받아온 것 저장
       localStorage.setItem(NAVER_USERNAME_KEY, username); // localStorage에 이름 값 저장
@@ -59,7 +45,6 @@ const savedNaverUsername = localStorage.getItem(NAVER_USERNAME_KEY);
 const savedNaverEmail = localStorage.getItem(NAVER_EMAIL_KEY);
 
 var testPopUp;
-<<<<<<< HEAD
 function openPopUp() {
   // 로그아웃 창 open
   console.log("hi");
@@ -72,14 +57,6 @@ function openPopUp() {
 }
 function closePopUp() {
   // 로그아웃 창 close
-=======
-function openPopUp() { // 로그아웃 창 open
-  console.log("hi");
-  testPopUp = window.open("https://nid.naver.com/nidlogin.logout", "_blank", "toolbar=yes,scrollbars=yes,resizable=no,width=1,height=1");
-  window.location.href;
-}
-function closePopUp() { // 로그아웃 창 close
->>>>>>> c58eede0bfa25f258deb496231d9d1e3fb42c88b
   testPopUp.close();
 }
 
@@ -98,12 +75,8 @@ function naverLogout() {
   if (savedNaverUsername === null && savedNaverEmail === null) {
     openPopUp(); // 로그아웃 창 open
     closePopUp();
-<<<<<<< HEAD
     setTimeout(function () {
       // 0.1초 뒤에 로그아웃 창 close
-=======
-    setTimeout(function () { // 0.1초 뒤에 로그아웃 창 close
->>>>>>> c58eede0bfa25f258deb496231d9d1e3fb42c88b
       closePopUp();
     }, 0);
     // alert("로그아웃 되었습니다.") // 로그아웃 성공
