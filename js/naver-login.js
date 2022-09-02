b5da3db7f058743dc4cccebf378a2ab90545c6bf
ksh990913@naver.com <ksh990913@naver.com>
2022-09-02 09:45:08 +0900

    수정

--- js/naver-login.js
+++ js/naver-login.js
@@ -2,7 +2,7 @@ var naverLogin = new naver.LoginWithNaverId({
   //내 애플리케이션 정보에 cliendId를 입력해줍니다.
   clientId: "wcyad8RUU4UMX2VrzXe2",
   // 내 애플리케이션 API설정의 Callback URL 을 입력해줍니다.
-  callbackUrl: "https://gksmftorsj.github.io/project0901/html/login-signup.html",
+  callbackUrl: "https://gksmftorsj.github.io/project0901/",
   isPopup: false, // 팝업을 통한 연동처리 여부
   callbackHandle: true, // callback 페이지가 분리되었을 경우에 callback 페이지에서는 callback 처리를 해줄 수 있도록 설정합니다.
 });
