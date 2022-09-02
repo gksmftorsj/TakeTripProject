c832ba9ff5b0b85cd204bdf15d7cb2c2ed502372
ksh990913@naver.com <ksh990913@naver.com>
2022-09-02 10:00:46 +0900

    수정

--- js/naver-login.js
+++ js/naver-login.js
@@ -24,10 +24,8 @@ window.addEventListener("load", function () {
       var email = naverLogin.user.getEmail(); // 필수정보인 이메일 값 받아온 것 저장
       localStorage.setItem(NAVER_EMAIL_KEY, email); // localStorage에 이메일 값 저장
       console.log("로그인 성공");
-      if (window.location == "https://gksmftorsj.github.io/project0901/html/login-signup.html") {
-        console.log("들어옴");
-        window.location.href = "https://gksmftorsj.github.io/project0901/";
-      } // 만약 로그인 페이지로 들어오면 메인페이지로 이동
+      window.location.href = "https://gksmftorsj.github.io/project0901/";
+      //만약 로그인 페이지로 들어오면 메인페이지로 이동
       if (username == undefined || username == null) {
         alert("이름은 필수정보입니다. 정보제공을 동의해주세요.");
         naverLogin.reprompt(); // 필수정보인 이름 값이 없다면 다시 한 번 동의창 띄우기
