622cf37ce2ab1c50e51d924b9500fc1643fee7f4
ksh990913@naver.com <ksh990913@naver.com>
2022-09-02 09:36:04 +0900

    수정

--- js/naver-login.js
+++ js/naver-login.js
@@ -2,9 +2,9 @@ var naverLogin = new naver.LoginWithNaverId({
   //내 애플리케이션 정보에 cliendId를 입력해줍니다.
   clientId: "wcyad8RUU4UMX2VrzXe2",
   // 내 애플리케이션 API설정의 Callback URL 을 입력해줍니다.
-  callbackUrl: "https://gksmftorsj.github.io/project0901/",
+  callbackUrl: "https://gksmftorsj.github.io/project0901/html/login-signup.html",
   isPopup: false, // 팝업을 통한 연동처리 여부
-  callbackHandle: true, // callback 페이지가 분리되었을 경우에 callback 페이지에서는 callback 처리를 해줄 수 있도록 설정합니다.02
+  callbackHandle: true, // callback 페이지가 분리되었을 경우에 callback 페이지에서는 callback 처리를 해줄 수 있도록 설정합니다.
 });
 
 // (3) 네이버아이디 로그인 정보를 초기화하기 위하여 init을 호출
@@ -24,7 +24,7 @@ window.addEventListener("load", function () {
       var email = naverLogin.user.getEmail(); // 필수정보인 이메일 값 받아온 것 저장
       localStorage.setItem(NAVER_EMAIL_KEY, email); // localStorage에 이메일 값 저장
       console.log("로그인 성공");
-      window.location.href;
+      // window.location.href;
       if (username == undefined || username == null) {
         alert("이름은 필수정보입니다. 정보제공을 동의해주세요.");
         naverLogin.reprompt(); // 필수정보인 이름 값이 없다면 다시 한 번 동의창 띄우기
