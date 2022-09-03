ed5fe5efb4f09a68c720728f11edbdaf290a71ca
gksmftorsj <ksh990913@naver.com>
2022-09-03 16:34:19 +0900

    수정

--- js/nav.js
+++ js/nav.js
@@ -64,7 +64,8 @@ function closePopUp() {
 }
 
 // 로그아웃 버튼 구현
-function handleLogoutBtn() {
+function handleLogoutBtn(event) {
+  event.preventDefault();
   if (KAKAO_USERNAME !== null && KAKAO_EMAIL !== null) {
     // 카카오 값 있으면 모두 지우고 로그아웃
     localStorage.removeItem("kakao_username");
