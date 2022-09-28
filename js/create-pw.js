e69df84177b2d7a489db9ef6449a8818d70379ba
ksh990913@naver.com <ksh990913@naver.com>
2022-09-28 17:11:08 +0900

    수정

--- js/create-pw.js
+++ js/create-pw.js
@@ -8,10 +8,7 @@ const EMAIL_KEY = "signup_email";
 const PW_KEY = "signup_pw";
 const PWCHK_KEY = "signup_pwChk";
 
-console.log("hyi")
-
-function createPw(e) {
-  e.preventDefault();
+function createPw() {
   // 만약 현재 비밀번호 없고 다 없으면 생성
   const username = localStorage.getItem("username_inUse");
   const email = localStorage.getItem("email_inUse");
@@ -23,26 +20,33 @@ function createPw(e) {
   const parsedPw = JSON.parse(localStorage.getItem(PW_KEY));
   const parsedPwChk = JSON.parse(localStorage.getItem(PWCHK_KEY));
 
-  console.log("1번 들어옴")
+  parsedUsername.push(username);
+  parsedEmail.push(email);
+  parsedPw.push(pw);
+  parsedPwChk.push(pwChk);
 
-  if (pw === "" && pwChk === "") {
-    alert("값을 입력해주세요.")
-  } else if (pw === pwChk && !parsedPw.includes(pw)) {
-    console.log("2번 들어옴")
-    parsedUsername.push(username);
-    parsedEmail.push(email);
-    parsedPw.push(pw);
-    parsedPwChk.push(pwChk);
+  localStorage.setItem(USERNAME_KEY, JSON.stringify(parsedUsername));
+  localStorage.setItem(EMAIL_KEY, JSON.stringify(parsedEmail));
+  localStorage.setItem(PW_KEY, JSON.stringify(parsedPw));
+  localStorage.setItem(PWCHK_KEY, JSON.stringify(parsedPwChk));
 
-    localStorage.setItem(USERNAME_KEY, JSON.stringify(parsedUsername));
-    localStorage.setItem(EMAIL_KEY, JSON.stringify(parsedEmail));
-    localStorage.setItem(PW_KEY, JSON.stringify(parsedPw));
-    localStorage.setItem(PWCHK_KEY, JSON.stringify(parsedPwChk));
-
-    localStorage.removeItem("kakaoLogin");
-    localStorage.removeItem("naverLogin");
-    window.close();
-  }
+  localStorage.removeItem("kakaoLogin");
+  localStorage.removeItem("naverLogin");
+  window.close();
 }
 
-create_pw_form.addEventListener("submit", createPw)
+create_pw_form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const pw = create_pw.value;
+  const pwChk = create_pwChk.value;
+  const parsedPw = JSON.parse(localStorage.getItem(PW_KEY));
+  if (pw === "" && pwChk === "") {
+    alert("값을 입력해주세요.")
+  } else if (pw === pwChk && parsedPw === null) {
+    console.log("hi");
+    createPw();
+  } else if (pw === pwChk && !parsedPw.includes(pw)) {
+    console.log("ㅈㅇㅂㅈㅇ");
+    createPw();
+  }
+})
