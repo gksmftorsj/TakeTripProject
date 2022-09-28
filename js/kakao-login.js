7bf89d8eed9e8649908b9e650a4fb3808d867d1b
gksmftorsj <ksh990913@naver.com>
2022-09-28 13:09:00 +0900

    수정

--- js/kakao-login.js
+++ js/kakao-login.js
@@ -37,8 +37,8 @@ function kakaoLogin() {
             "username_inUse",
             kakao_account.profile.nickname
           );
-          localStorage.setItem("email_inUse", kakao_account.email);
-          localStorage.setItem("kakaoLogin", kakao);
+          localStorage.setItem("email_inUse", "kakao_account.email");
+          localStorage.setItem("kakaoLogin", "kakao");
           console.log("로그인 성공");
           window.location.href = "https://gksmftorsj.github.io/project0901/";
         },
