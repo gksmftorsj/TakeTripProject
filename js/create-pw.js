6ad352c0e00081c79e4da0e495a50796c915997c
ksh990913@naver.com <ksh990913@naver.com>
2022-09-28 17:19:48 +0900

    수정

--- js/create-pw.js
+++ js/create-pw.js
@@ -8,6 +8,12 @@ const EMAIL_KEY = "signup_email";
 const PW_KEY = "signup_pw";
 const PWCHK_KEY = "signup_pwChk";
 
+let arr_username = [];
+let arr_email = [];
+let arr_pw = [];
+let arr_pwChk = [];
+
+
 function createPw() {
   // 만약 현재 비밀번호 없고 다 없으면 생성
   const username = localStorage.getItem("username_inUse");
@@ -20,16 +26,25 @@ function createPw() {
   const parsedPw = JSON.parse(localStorage.getItem(PW_KEY));
   const parsedPwChk = JSON.parse(localStorage.getItem(PWCHK_KEY));
 
-  parsedUsername.push(username);
-  parsedEmail.push(email);
-  parsedPw.push(pw);
-  parsedPwChk.push(pwChk);
-
-  localStorage.setItem(USERNAME_KEY, JSON.stringify(parsedUsername));
-  localStorage.setItem(EMAIL_KEY, JSON.stringify(parsedEmail));
-  localStorage.setItem(PW_KEY, JSON.stringify(parsedPw));
-  localStorage.setItem(PWCHK_KEY, JSON.stringify(parsedPwChk));
-
+  if (parsedUsername === null) {
+    arr_username.push(username);
+    arr_email.push(email);
+    arr_pw.push(pw);
+    arr_pwChk.push(pwChk);
+    localStorage.setItem(USERNAME_KEY, JSON.stringify(arr_username));
+    localStorage.setItem(EMAIL_KEY, JSON.stringify(arr_email));
+    localStorage.setItem(PW_KEY, JSON.stringify(arr_pw));
+    localStorage.setItem(PWCHK_KEY, JSON.stringify(parsedPwarr_pwChkChk));
+  } else {
+    parsedUsername.push(username);
+    parsedEmail.push(email);
+    parsedPw.push(pw);
+    parsedPwChk.push(pwChk);
+    localStorage.setItem(USERNAME_KEY, JSON.stringify(parsedUsername));
+    localStorage.setItem(EMAIL_KEY, JSON.stringify(parsedEmail));
+    localStorage.setItem(PW_KEY, JSON.stringify(parsedPw));
+    localStorage.setItem(PWCHK_KEY, JSON.stringify(parsedPwChk));
+  }
   localStorage.removeItem("kakaoLogin");
   localStorage.removeItem("naverLogin");
   window.close();
